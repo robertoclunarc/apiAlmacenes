567670e8d2075508da5db07012b75c756fb7e988
Yamil Lanz <ylanz@2050net.local>
2020-12-04 08:28:58 -0400

    probando variables de entorno gitlab

--- app/src/database.ts
+++ app/src/database.ts
@@ -4,8 +4,6 @@ class database {
 
     cnn: any;
 
-
-
     async conectarBD() {
         /*    this.cnn.connect((err) => {
                if (err) throw err;
@@ -13,11 +11,19 @@ class database {
            }); */
         //await this.cnn.query
         this.cnn = await mysql.createPool({
-            host:  process.env.MYSQL_SERVER || "localhost", //"10.10.0.7",
-            user: process.env.MYSQL_USER || "root",
-            password: process.env.MYSQL_PW || ".4C3r04dm1n", //"4c3r04dm1n",
-            database: process.env.MYSQL_DB || "intranet"
+            host: process.env.MYSQL_SERVER, // || "localhost", //"10.10.0.7",
+            user: process.env.MYSQL_USER, // || "root",
+            password: process.env.MYSQL_PW, // || ".4C3r04dm1n", //"4c3r04dm1n",
+            database: process.env.MYSQL_DB, // || "intranet"
         }).getConnection();
+
+        try {
+            let testconection = await this.cnn.query(`use ${process.env.MYSQL_DB};`);
+            console.log(`Database ${process.env.MYSQL_DB} conected!`);
+        } catch (error) {
+            console.log(`ERROR database conection!: ${error} `);
+        }
+
     }
 
     getC() {
@@ -29,10 +35,10 @@ class database {
     }
 
     async querySelect(sql: string, data?: any) {
-        
-        let result :any = null;
+
+        let result: any = null;
         if (!data) {
-             result = await this.cnn.query(sql);
+            result = await this.cnn.query(sql);
         } else {
             result = await this.cnn.query(sql, data);
         }
