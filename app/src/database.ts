bd3130d7ee045c0020472f9765e9d21a7a3e2009
Yamil Lanz <ylanz@2050net.local>
2021-01-22 11:55:26 -0400

    Cambio de promise-mysql2 a promise-mysql por mejoras en el query-release

--- app/src/database.ts
+++ app/src/database.ts
@@ -1,4 +1,4 @@
-import mysql from "mysql2/promise";
+import mysql from "promise-mysql";
 
 class database {
 
@@ -22,12 +22,16 @@ class database {
                console.log("Database is connected!");
            }); */
         //await this.cnn.query
-        this.cnn = await mysql.createPool({
+        let pool = await mysql.createPool({
             host: this.host, //process.env.MYSQL_SERVER || "localhost", //"10.10.0.7",
             user: this.user, // process.env.MYSQL_USER || "root",
             password: this.pass, // process.env.MYSQL_PW || ".4C3r04dm1n", //"4c3r04dm1n",
             database: this.database, // process.env.MYSQL_DB || "intranet"
-        }).getConnection();
+            connectTimeout: 10000,
+            connectionLimit: 2
+        });
+
+        this.cnn = pool;
 
         try {
             let testconection = await this.cnn.query(`use ${this.database};`);
@@ -55,10 +59,12 @@ class database {
         } else {
             result = await this.cnn.query(sql, data);
         }
-        await this.cnn.release();
+        //await this.cnn.release();
         //this.cnn = null;
-        this.desconectarDB();
-        return result[0];
+       // this.desconectarDB();
+       //console.log(result);
+       
+        return result;
     }
 
     async inuup() {
